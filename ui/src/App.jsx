722f21c51325d84402ef54ddebe9f88c69255622
rafael <yhrapha@gmail.com>
2025-05-07 12:25:55 +0100

    tests

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -4,7 +4,17 @@ import DefaultLayout from "./layouts/DefaultLayout";
 import { createRoutes, routes } from "./routes/routes";
 import { ToastContainer } from "react-toastify";
 
+export const getSubdomain = () => {
+  const host = window.location.hostname;
+  console.log(host);
+  const parts = host.split(".");
+  const subdomain = parts.length === 2 ? parts[0] : null;
+  return subdomain;
+};
+
 const App = () => {
+  const subdomian = getSubdomain();
+  console.log(subdomian);
   return (
     <div className="">
       <ToastContainer position="top-right" autoClose={3000} />
